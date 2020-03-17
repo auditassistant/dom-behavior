2a3f2796a9a622fdbc7c9c0b9686f6b9f409a29d
Swikrit <swikrit@auditassistant.com>
2020-03-18 12:10:15 +1300

    Standard

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-module.exports = function(behaviors, rootElement, options){
+module.exports = function (behaviors, rootElement, options) {
   // options: attribute, property
   options = options || {}
   options.attribute = options.attribute || 'data-behavior'
   options.property = options.property || 'behaviors'
 
-  if (!rootElement){
+  if (!rootElement) {
     rootElement = document
   }
 
-
-  function apply(rootElement, recursive){
-    walkDom(rootElement, function(node){
-
-      if (!recursive && node !== rootElement){
+  function apply (rootElement, recursive) {
+    walkDom(rootElement, function (node) {
+      if (!recursive && node !== rootElement) {
         return false // break
-      } else if (recursive === 'inner' && node === rootElement){
+      } else if (recursive === 'inner' && node === rootElement) {
         return // continue
       }
 
-      if (node.getAttribute && node.getAttribute(options.attribute)){
-        if (!node[options.property]){
+      if (node.getAttribute && node.getAttribute(options.attribute)) {
+        if (!node[options.property]) {
           node[options.property] = node[options.property] || {}
         }
 
@@ -27,21 +25,21 @@ module.exports = function(behaviors, rootElement, options){
         var nodeBehaviors = node[options.property]
 
         // trigger updates or removes
-        Object.keys(nodeBehaviors).forEach(function(key){
+        Object.keys(nodeBehaviors).forEach(function (key) {
           var current = nodeBehaviors[key]
-          if (~behaviorNames.indexOf(key)){
-            if (typeof current == 'function') current.call(node, 'change')
+          if (~behaviorNames.indexOf(key)) {
+            if (typeof current === 'function') current.call(node, 'change')
           } else {
             ;delete nodeBehaviors[key]
-            if (typeof current == 'function') current.call(node, 'remove')
+            if (typeof current === 'function') current.call(node, 'remove')
           }
         })
 
         // add new behaviors
-        behaviorNames.forEach(function(name){
-          if (!(name in nodeBehaviors)){
+        behaviorNames.forEach(function (name) {
+          if (!(name in nodeBehaviors)) {
             var behavior = getBehavior(name, behaviors)
-            if (behavior){
+            if (behavior) {
               nodeBehaviors[name] = behavior(node)
             }
           }
@@ -52,27 +50,26 @@ module.exports = function(behaviors, rootElement, options){
 
   apply(rootElement, true)
 
-  return function(change, node){
-    if (change === 'remove'){
+  return function (change, node) {
+    if (change === 'remove') {
       remove(node, options)
-    } else if (change === 'inner'){
+    } else if (change === 'inner') {
       apply(node, 'inner')
-    } else if (change === 'update'){
+    } else if (change === 'update') {
       return apply(node, false)
     } else { // append
       apply(node, true)
     }
   }
-
 }
 
-var remove = module.exports.remove = function(rootElement, options){
-  walkDom(rootElement, function(node){
-    if (node.getAttribute && node.getAttribute(options.attribute) && node[options.property]){
+var remove = module.exports.remove = function (rootElement, options) {
+  walkDom(rootElement, function (node) {
+    if (node.getAttribute && node.getAttribute(options.attribute) && node[options.property]) {
       var nodeBehaviors = node[options.property]
-      Object.keys(nodeBehaviors).forEach(function(key){
+      Object.keys(nodeBehaviors).forEach(function (key) {
         var current = nodeBehaviors[key]
-        if (typeof current == 'function'){
+        if (typeof current === 'function') {
           current.call(node, 'remove')
         }
       })
@@ -81,13 +78,13 @@ var remove = module.exports.remove = function(rootElement, options){
   })
 }
 
-function getBehavior(name, behaviors){
-  if (behaviors[name]){
+function getBehavior (name, behaviors) {
+  if (behaviors[name]) {
     return behaviors[name]
-  } else if (~name.indexOf('.')){ // nested behaviors
+  } else if (~name.indexOf('.')) { // nested behaviors
     var parts = name.split('.')
-    for (var i=0;i<parts.length;i++){
-      if (behaviors && behaviors[parts[i]]){
+    for (var i = 0; i < parts.length; i++) {
+      if (behaviors && behaviors[parts[i]]) {
         behaviors = behaviors[parts[i]]
       } else {
         behaviors = null
@@ -97,20 +94,19 @@ function getBehavior(name, behaviors){
   }
 }
 
-function walkDom(rootNode, iterator){
+function walkDom (rootNode, iterator) {
   var currentNode = rootNode
-  while (currentNode){
-    if (iterator(currentNode) === false){
+  while (currentNode) {
+    if (iterator(currentNode) === false) {
       break // early escape
     }
-    if (currentNode.firstChild){
+    if (currentNode.firstChild) {
       currentNode = currentNode.firstChild
-    } else if (currentNode === rootNode){
+    } else if (currentNode === rootNode) {
       currentNode = null
     } else {
-      
-      while (currentNode && !currentNode.nextSibling){
-        if (currentNode.parentNode !== rootNode){
+      while (currentNode && !currentNode.nextSibling) {
+        if (currentNode.parentNode !== rootNode) {
           currentNode = currentNode.parentNode
         } else {
           currentNode = null
